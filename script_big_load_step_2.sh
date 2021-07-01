7730f5c267b3ecdc58fb6fef42fbb013d9020761
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-01 19:51:16 -0400

    Update script_big_load_step_2.sh

--- script_big_load_step_2.sh
+++ script_big_load_step_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cat temp.json | jq '[.[] |{image_name: .instances[].image, package_info: .packages[].pkgs[]}]' \
+cat ./temp.json | jq '[.[] |{image_name: .instances[].image, package_info: .packages[].pkgs[]}]' \
 | jq 'group_by(.image_name)[] | {image_name: .[0].image_name, package_info: [.[].package_info | {package_name: .name,version: .version,license: .license }]}' \
 | jq '[{(.image_name): .package_info[]}]' \
 | grep -i -P -B 2 -A 2 "${pcee_package}" > "$(date  +%m_%d_%y)_container_images_with_${pcee_package}.txt"
