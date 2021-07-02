95ad6b440272feecb3a2164f60c85a76a84b0cb7
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-01 22:30:27 -0400

    Update script_big_load_step_4.sh

--- script_big_load_step_4.sh
+++ script_big_load_step_4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ./script_big_load_step_1.sh
+echo "okay now enter the package you'd like to find"
+read -r pcee_package;
 
 awk -v pcee_package="${pcee_package}" -F, '$2 ~/pcee_package/ {print}' ./temp_report_$(date  +%m_%d_%y).csv > ./report_$(date  +%m_%d_%y)_containers_with_${pcee_package}.csv
 
