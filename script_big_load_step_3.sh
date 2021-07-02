488b0822f6d65e06cf85c631e0954ceb5b75a7f4
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-01 23:06:31 -0400

    Update script_big_load_step_3.sh

--- script_big_load_step_3.sh
+++ script_big_load_step_3.sh
@@ -4,8 +4,9 @@
 cat ./report_$(date  +%m_%d_%y).csv | awk 'NR == 1 || NR % 2 == 0' > temp_report_$(date  +%m_%d_%y).csv
 
 # time to clean
-
-rm ./temp.json
+if [[ -f ./temp.json ]]; then
+  rm ./temp.json
+fi
 
 bash ./script_big_load_step_4.sh
 
