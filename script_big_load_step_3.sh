79613398659b04c89e11ea31894ef01fa1713935
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-02 13:39:41 -0400

    Update script_big_load_step_3.sh

--- script_big_load_step_3.sh
+++ script_big_load_step_3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cat ./report_$(date  +%m_%d_%y).csv | awk 'NR == 1 || NR % 2 == 0' > temp_report_$(date  +%m_%d_%y).csv
+cat ./report_$(date  +%m_%d_%y).csv | awk 'NR == 1 || NR % 2 == 0' > ./temp_report_$(date  +%m_%d_%y).csv
 
 # time to clean
 if [[ -f ./temp.json ]]; then
