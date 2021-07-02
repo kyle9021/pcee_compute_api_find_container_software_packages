9641823e8444f48a40f9adc2827ede3892504346
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-02 13:37:52 -0400

    Update container_licenses_and_packages.sh

--- container_licenses_and_packages.sh
+++ container_licenses_and_packages.sh
@@ -159,7 +159,7 @@ pcee_container_package_info=$(for pcee_offset in $(seq 0 ${pcee_compute_api_limi
 printf %s "${pcee_container_package_info}" | jq '[.[] | {image_name: .instances[].image, package_info: .packages[].pkgs[]} ]' \
 | jq 'group_by(.image_name)[] | {image_name: .[0].image_name, package_info: [.[].package_info | {name: .name, version: .version, license: .license}]} | {image_name: .image_name, package_info: .package_info[]}' \
 | jq '[. | {image_name: .image_name, name: .package_info.name, version: .package_info.version, license: .package_info.license}]' \
-| jq '[.[]]' |jq -r 'map({image_name,name,version,license}) | (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows |$keys,$rows[]| @csv' > report_$(date  +%m_%d_%y).csv
+| jq '[.[]]' |jq -r 'map({image_name,name,version,license}) | (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows |$keys,$rows[]| @csv' > ./report_$(date  +%m_%d_%y).csv
 
 
 
