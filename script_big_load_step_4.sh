0b79b8534e15769d090f628a2aae64159ef0fd55
Kyle Butler <54778108+Kyle9021@users.noreply.github.com>
2021-07-01 22:32:13 -0400

    Update script_big_load_step_4.sh

--- script_big_load_step_4.sh
+++ script_big_load_step_4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Found in the Prisma Console Under: Compute > Vulnerabilities on the image tab; click the image and then go to the Package Info tab
-echo "enter the name of the software package you're looking for, partial matches okay, and perl regex works too, CASE-SENSITIVE"
-echo "found in the compute console under: compute > vulnerabilities on the image tab"
+echo "Enter the name of the software package you're looking for, partial matches okay, and perl regex works too, NOT case-sensitive"
+echo "Found in the compute console under: compute > vulnerabilities on the image tab"
 read -r pcee_package;
 
 awk -v pcee_package="${pcee_package}" -F, '$2 ~/pcee_package/ {print}' ./temp_report_$(date  +%m_%d_%y).csv > ./report_$(date  +%m_%d_%y)_containers_with_${pcee_package}.csv
